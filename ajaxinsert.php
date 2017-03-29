c39c431365fe58789cbb1f792f9b055dc8316b39
Davide Bonifacio - Studiokey <web@davidebonifacio.it>
2017-03-29 15:27:53 +0200

    adding date stamp when inserting item

--- ajaxinsert.php
+++ ajaxinsert.php
@@ -2,15 +2,16 @@
   session_start();
   include_once 'dbconnect.php';
 
-  $description = $amount = $table = "";
+  $description = $amount = $table = $date = "";
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $description = htmlspecialchars($_POST['description']);
     $amount = htmlspecialchars($_POST['amount']);
     $table = htmlspecialchars($_POST['table']);
+    $date = htmlspecialchars($_POST['table']);
   }
 
-  $sql7 = "INSERT INTO $table (description, amount)
-  VALUES ('$description', '$amount')";
+  $sql7 = "INSERT INTO $table (description, amount, date)
+  VALUES ('$description', '$amount', NOW())";
 
   if ($con->query($sql7) === TRUE) {
     echo "<p>New item created successfully</p>";
